734fdd17f88903885eb266a036e6e5e8f973e5ed
oxdc <frnet2016@gmail.com>
2018-08-06 13:00:42 +0800

    update config-example

--- config-example.js
+++ config-example.js
@@ -1,10 +1,23 @@
-const configuration = {
-    host: '127.0.0.1',
-    port: 8080,
-    wikiDir: '/absolute/path/to/your/wiki', // CHANGE ME !!
-    username: 'user', // CHANGE ME !!
-    password: 'passw0rd', // CHANGE ME !!
+  const configuration = {
+    host: '127.0.0.1', // localhost or 0.0.0.0 for web access
+    port: 8080, // server port
+    wikiDir: '/absolute/path/to/your/wiki',
+    /*
+      You must change this path to your wiki's
+      absolute path
+    */
+    username: 'user', // CHANGE ME OR DELETE !!
+    password: 'passw0rd', // CHANGE ME  OR DELETE !!
+    /*
+      NOTE: If you delete two lines above,
+      your wiki site will be open to all!
+      Anyone can visit and edit without login!
+    */
     autoLogin: true
+    /*
+      If true, you won't need to enter your
+      password each time you open this application.
+    */
 }
 
 module.exports = configuration
